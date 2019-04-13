e9f58a0268dd5b43908cc6a23f906c5c587641d2
zombi3123 <joshuacarpenterjones@gmail.com>
2019-04-13 09:15:34 +0100

    Update BallPanel.java

--- src/BallPanel.java
+++ src/BallPanel.java
@@ -45,7 +45,7 @@ public class BallPanel extends JPanel implements ActionListener {
                     //ball.xSpeed=randNum.nextInt(1);
                     //ball.ySpeed=randNum.nextInt(1);
                     double n = 1;
-                    ball.setMass(randNum.nextInt(10));
+                    ball.setMass(1);
                     ball.xSpeed=n;
                     ball.ySpeed=n;
                     balls.add(ball);
