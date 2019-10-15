b64ce170c07ee69f109e575848c6859bd668691b
zombi3123 <joshuacarpenterjones@gmail.com>
2019-10-15 23:21:03 +0100

    Delete MainClass.java

--- src/MainClass.java
+++
@@ -1,8 +0,0 @@
-public class MainClass {
-    public static void main(String[] args){
-
-        Window w=new Window(1000,1000);
-        BallPanel bp=new BallPanel(w.getWidth()-50,w.getHeight()-50);
-        w.add(bp);
-    }
-}
